eb4da9456c717306f22c233cdaa92b669155fb0a
Alex Barakhov <abarakhov@gmail.com>
2023-01-24 17:51:18 +0100

    added panel drag resizer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
 import Button from "./components/Button";
 import Stack from "./components/Stack";
-import './App.css';
+import ResizePanel from "react-resize-panel";
+import style from './App.css';
 
 function App() {
   function handleClick() {
     console.log('dale don dale');
   }
   return (
-    <>
-      <Button
-        label="lolo"
-        backgroundColor="green"
-        size="lg"
-        onClick={handleClick}
-      />
-      <Button
-        label="lolo2"
-        size="sm"
-      />
-      <Stack>
-        <div className="stack-item">1</div>
-        <div className="stack-item">2</div>
-        <div className="stack-item">3</div>
-        <div className="stack-item">4</div>
-        <div className="stack-item">5</div>
-      </Stack>
-    </>
+
+      <div className="mainContainer">
+
+        <ResizePanel
+          direction="e"
+          style={{ width: "25%", minWidth: "200px", }}
+        >
+          <aside className="aside">
+
+            <Stack wrap>
+              <div>
+              <Button
+                label="button"
+                backgroundColor="green"
+                size="lg"
+                onClick={handleClick}
+              />
+              </div>
+
+              <div className="stack-item">box</div>
+              <div>
+
+              <Button
+                label="Button 2"
+                size="sm"
+              />
+              </div>
+              <div className="stack-item">box</div>
+            </Stack>
+          </aside>
+        </ResizePanel>
+
+        <main className="main">
+        <ResizePanel
+          direction="e"
+          style={{ width: "50%", maxWidth: "70vw",}}
+        >
+          <div className="sections">
+            <section className="section section--1">Drop 1</section>
+            <section className="section section--2">Drop 2</section>
+            <section className="section section--3">Drop 3</section>
+          </div>
+          </ResizePanel>
+          <div className="table"></div>
+
+        </main>
+
+      </div>
+
+
+
+
   );
 }
 
