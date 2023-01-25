87985f0c9908f2be7389c02abc9c3fe03552df38
Alex Barakhov <abarakhov@gmail.com>
2023-01-25 15:03:02 +0100

    layout styling

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
 import Button from "./components/Button";
 import Stack from "./components/Stack";
 import ResizePanel from "react-resize-panel";
-import style from './App.css';
+import './App.css';
 
 function App() {
+
   function handleClick() {
     console.log('dale don dale');
   }
-  return (
 
-      <div className="mainContainer">
+  return (
+<>
+    <header className="header">
+      <Stack wrap>
+        <Button label="Button"/>
+        <Button label="Button 2"/>
+        <Button
+          label="Button 3"
+          backgroundColor="lightgreen"
+          onClick={handleClick}
+        />
+      </Stack>
+    </header>
+
+    <div className="panelsContainer">
 
         <ResizePanel
           direction="e"
-          style={{ width: "25%", minWidth: "200px", }}
+          style={{ width: "290px", minWidth: "160px", maxWidth: "49vw",}}
         >
           <aside className="aside">
 
-            <Stack wrap>
-              <div>
-              <Button
-                label="button"
-                backgroundColor="green"
-                size="lg"
-                onClick={handleClick}
-              />
-              </div>
-
-              <div className="stack-item">box</div>
-              <div>
-
-              <Button
-                label="Button 2"
-                size="sm"
-              />
-              </div>
-              <div className="stack-item">box</div>
-            </Stack>
+            <div className="card"></div>
           </aside>
         </ResizePanel>
 
         <main className="main">
         <ResizePanel
           direction="e"
-          style={{ width: "50%", maxWidth: "70vw",}}
+          style={{ width: "30vw", minWidth: "240px", maxWidth: "50vw",}}
         >
           <div className="sections">
             <section className="section section--1">Drop 1</section>
@@ -55,10 +50,8 @@ function App() {
 
         </main>
 
-      </div>
-
-
-
+    </div>
+</>
 
   );
 }
